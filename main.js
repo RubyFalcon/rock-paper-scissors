428d2800a65a69e2358dc430aa407ec288ee8248
Tupay Felkin <tupai@live.co.uk>
2024-11-20 16:43:10 +0000

    create function playRound for rock,paper,scissors

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // FUNCTION getComputerChoice
+// gets x as random numbner between 0 and 2
+// assign 0 to 'rock', 1 to 'paper', 2 to scissors using IF statements
+// return our random choice
 
 function getComputerChoice() {
     let x = Math.floor(Math.random()* 3) ;
@@ -15,11 +18,16 @@ function getComputerChoice() {
     return choice;
 }
 
+// GET input from human
+// LOOP forever until human provides either 'rock' , 'paper' or 'scissors'
+// IF input is allowed, return humanChoice
+// ELSE alert use that their input is incorrect and to try again
 function getHumanChoice() {
     let  keepGoing =true;
     
     while(keepGoing){
         let humanChoice = prompt("choose 'rock' , 'paper' or 'scissors");
+        humanChoice = humanChoice.toLowerCase();
         if (humanChoice == 'rock' || humanChoice =='paper' || humanChoice == 'scissors') {
             
             keepGoing = false;
@@ -31,6 +39,34 @@ function getHumanChoice() {
     }
     
 }
+
+//GET humanScore and computerScore variables, INITIALIZE to 0
+let humanScore = 0;
+let computerScore = 0;
+
+
+//FUNCTION playRound()
+// compare choices and return a winner
+function playRound(humanChoice, computerChoice){
+   
+     if(humanChoice == computerChoice){
+        
+        console.log("Its a draw");
+     }
+     else if((humanChoice== 'rock' && computerChoice == 'scissors') || (humanChoice == 'paper' && computerChoice == 'rock' ) || (humanChoice == 'scissors' && computerChoice == 'paper')){
+        console.log("Congrats you won!");
+        humanScore += 1;
+     }
+     else {
+        console.log('You lost, unlucky')
+        computerScore += 1;
+     }
+     console.log(`human: ${humanScore}, computer: ${computerScore}`);
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
 //INPUT GET choice of type string
 // opponentChoice of type string = function randomChoice
 //CASE  of choice:
